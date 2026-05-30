12d2226c10ef7be024fd0bf5b334b9f39d8f9390
Prateek Mishra <75943911+Prateek692@users.noreply.github.com>
2026-05-30 15:57:52 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        occurence_indices.clear();
+        vector<int>res = {0,0};
+        for(int i=0;i<nums.size();i++)
+            occurence_indices[nums[i]].push_back(i);
+        
+        for(const auto ele:occurence_indices){
+            int diff = target - ele.first;
+            if(occurence_indices.find(diff)!=occurence_indices.end()){
+                if(diff==ele.first)
+                    res = {ele.second[0],ele.second[1]};
+                else{
+                    int a = min(ele.second[0],occurence_indices[diff][0]);
+                    int b = max(ele.second[0],occurence_indices[diff][0]);
+                    res = {a,b};
+                }
+                return res;
+            }   
+        }
+        return res;
+    }
+    map<int,vector<int>>occurence_indices;
+};
