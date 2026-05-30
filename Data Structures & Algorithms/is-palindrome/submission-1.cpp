9d626ca2f067674c4954e55f4c16b05726bd0b29
Prateek Mishra <75943911+Prateek692@users.noreply.github.com>
2026-05-30 21:56:50 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        filterString(s);
+        int p1=0;
+        int p2=s.size()-1;
+        while(p1<p2){
+            if(s[p1]!=s[p2])
+                return false;
+            p1++;
+            p2--;
+        }
+        return true;
+    }
+private:
+    void filterString(string &s){
+        string t = "";
+        for(auto c:s){
+            if((c>='0' and c<='9') or (c>='a' and c<='z') or (c>='A' and c<='Z')){
+                if(c>='A' and c<='Z')
+                    c+=32;
+                t+=c;
+            }
+                
+        }
+        s=t;
+    }
+};
